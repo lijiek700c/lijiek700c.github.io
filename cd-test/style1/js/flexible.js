3f11aa1638c6d0028d76ddfceb85deb615665ad8
lijiek700c <diaosialone@sina.com>
2017-07-23 08:50:12 +0800

    my page

--- cd-test/style1/js/flexible.js
+++ cd-test/style1/js/flexible.js
@@ -35,7 +35,7 @@
         var isAndroid = win.navigator.appVersion.match(/android/gi);
         var isIPhone = win.navigator.appVersion.match(/iphone/gi);
         var devicePixelRatio = win.devicePixelRatio;
-        if (isIPhone) {
+        //if (isIPhone) {
             // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
             if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {                
                 dpr = 3;
@@ -44,10 +44,10 @@
             } else {
                 dpr = 1;
             }
-        } else {
+        //} else {
             // 其他设备下，仍旧使用1倍的方案
-            dpr = 1;
-        }
+            //dpr = 1;
+        //}
         scale = 1 / dpr;
     }
 
