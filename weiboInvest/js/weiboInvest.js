0561e288d016cc3bed7902133cbda725c2e9a83e
lijiek700c <diaosialone@sina.com>
2018-02-10 10:47:00 +0800

    my page

--- weiboInvest/js/weiboInvest.js
+++ weiboInvest/js/weiboInvest.js
@@ -166,6 +166,54 @@ __webpack_require__(1);
 		};
 		return openLoading;
 	}($);
+	/*等待操作时*/
+	var dsWaitLoading = function ($) {
+		function waitLoading() {
+			this.txt = '正在加载';
+			this.html = '<div class="ds-loadingBox js_ds-loadingBox" style="background:rgba(0,0,0,0.2);">' + '<div class="item-loader-container wait">' + '<div class="la-fire la-2x">' + '<div></div>' + '<div></div>' + '<div></div>' + '</div><br>' + '<p>' + this.txt + '</p>' + '</div>' + '</div>';
+		}
+		waitLoading.prototype = {
+			constructor: waitLoading,
+			_show: function _show(txt, type) {
+				if (txt && $.type(txt) !== 'string') {
+					console.warn('字符串');
+					return;
+				}
+				if (dsLoadingObj) {
+					this.destroy();
+				}
+				dsLoadingObj = this;
+				if ($.type(txt) !== 'undefined') {
+					this.txt = txt;
+				}
+				switch (type) {
+					default:
+						$(this.html).find('p').text(this.txt).end().appendTo('body');
+						break;
+				}
+				return this;
+			},
+			_hide: function _hide(fn) {
+				if (fn && $.type(fn) !== 'function') {
+					console.warn('function');
+					return;
+				}
+				var plTimer = setTimeout($.proxy(function () {
+					clearTimeout(plTimer);
+					this.destroy();
+					fn && fn();
+				}, this), 200);
+				return this;
+			},
+			destroy: function destroy() {
+				$('.js_ds-loadingBox').fadeOut(200, function () {
+					$(this).remove();
+				});
+				dsLoadingObj = null;
+			}
+		};
+		return waitLoading;
+	}(jQuery);
 	/*提示的弹窗*/
 	var dsDialog = function ($) {
 		var oldDsDialog;
@@ -243,17 +291,17 @@ __webpack_require__(1);
 		};
 		return Dialog;
 	}($);
-	var _dialog = new dsDialog();
 	var dsOpenLoading = new dsOpenLoading();
+	var dsWaitLoading = new dsWaitLoading();
 	var _dialog = new dsDialog();
 	window.pg = {
 		dsOpenLoading: dsOpenLoading,
+		dsWaitLoading: dsWaitLoading,
 		dialog: function dialog(config) {
 			return _dialog.init(config);
 		}
 	};
 })(jQuery);
-
 /*设置rem*/
 var setRem = function () {
 	var docEle = window.document.documentElement;
@@ -353,22 +401,26 @@ var inputData = [{
 	title: '姓名',
 	beizhu: '',
 	red: '*',
-	placeholder: '请输入您的姓名'
+	placeholder: '请输入您的姓名',
+	type: 'name'
 }, {
 	title: '公司电话',
 	beizhu: '(手机)',
 	red: '*',
-	placeholder: '请输入您的电话号码'
+	placeholder: '请输入您的电话号码',
+	type: 'tel'
 }, {
-	title: '企业名称',
+	title: '企业全称',
 	beizhu: '',
 	red: '*',
-	placeholder: '请输入文本信息'
+	placeholder: '请输入文本信息',
+	type: ''
 }, {
-	title: '企业微博名',
+	title: '企业微博昵称',
 	beizhu: '',
 	red: '*',
-	placeholder: '请输入文本信息'
+	placeholder: '请输入文本信息',
+	type: ''
 }];
 $(function () {
 	pg.dsOpenLoading._show();
@@ -379,14 +431,235 @@ $(function () {
 		el: '.userSelectArea',
 		data: {
 			selectData: [],
-			inputData: []
+			inputData: [],
+			selectedData: {
+				'1': {},
+				'2': {},
+				'3': {},
+				'4': {}
+			},
+			telReg: /^((1[3|5|7|8][0-9])\d{8})|((0[1-9][0-9])\d{8})|((0[1-9][0-9])\d{7})|((0[1-9][0-9][0-9])\d{8})|((0[1-9][0-9][0-9])\d{7})$/,
+			nameReg: /^[\u4e00-\u9fa5]{1}/
 		},
 		mounted: function mounted() {
 			this.selectData = selectData;
 			this.inputData = inputData;
 			pg.dsOpenLoading.imgLoadedHide();
 		},
-		methods: {}
+		methods: {
+			selectAnswer: function selectAnswer(ev) {
+				var listItem = $(ev.currentTarget),
+				    listItemParent = listItem.parent(),
+				    itemCheckBox = listItem.find('input:checkBox'),
+				    itemIcon = listItem.find('img');
+				var multipleBool = listItemParent.attr('data-multiple'); //是否可多选
+				var order = parseInt(listItemParent.attr('data-order')); //问题序号
+				var aOrder = parseInt(listItem.attr('data-aOrder')); //答案序号
+				var checkedBool = itemCheckBox.prop('checked'); //选中不选中切换
+				var answer = listItem.find('span').map(function (i, e) {
+					return $(e).text();
+				}).get().join(''); //选中的答案信息
+				checkedBool ? itemCheckBox.prop('checked', '').siblings('img').attr('src', 'images/notSelect.png') : itemCheckBox.prop('checked', 'true').siblings('img').attr('src', 'images/selected.png');
+				if (multipleBool === '') {
+					listItem.siblings().find('input:checkBox').prop('checked', '').end().find('img').attr('src', 'images/notSelect.png');
+				}
+				/*存选中的数据*/
+				this.selectedDataOpe({
+					multipleBool: multipleBool,
+					bool: checkedBool,
+					order: order,
+					aOrder: aOrder,
+					answer: answer
+				});
+			},
+			selectedDataOpe: function selectedDataOpe(opt) {
+				if (opt.multipleBool !== '') {
+					opt.bool ? this.selectedData[opt.order][opt.aOrder] = '' : this.selectedData[opt.order][opt.aOrder] = opt.answer;
+				} else {
+					opt.bool ? this.selectedData[opt.order][1] = '' : this.selectedData[opt.order][1] = opt.answer;
+				}
+			},
+			inputFocus: function inputFocus(ev) {
+				var oInput = $(ev.currentTarget);
+				oInput.parent().css('border-color', '#6c85bc');
+			},
+			inputBlur: function inputBlur(ev) {
+				var oInput = $(ev.currentTarget);
+				oInput.parent().css('border-color', '#f6f6f6');
+			},
+			infosIsNotEmpty: function infosIsNotEmpty(eles) {
+				//输入信息全部不为空
+				var eleArr = $.makeArray(eles);
+				return eleArr.every(function (item) {
+					return item.value !== '';
+				});
+			},
+			selectedAnswerIsNotEmpty: function selectedAnswerIsNotEmpty() {
+				if (this.AnswerIsEmpty()) {
+					//只要有一个为空
+					return false;
+				};
+				var answerArr = $.map(this.selectedData, function (item, k) {
+					return item;
+				});
+				var emptyCount = 0;
+				$.each(answerArr, function (i, item) {
+					$.each(item, function (k, v) {
+						if (v !== '') {
+							emptyCount++;
+							return false;
+						}
+					});
+				});
+				return emptyCount === answerArr.length ? true : false;
+			},
+			AnswerIsEmpty: function AnswerIsEmpty() {
+				var answersList = $.map(this.selectedData, function (item, k) {
+					return item;
+				});
+				return answersList.some(function (item) {
+					return $.isEmptyObject(item);
+				});
+			},
+			submitData: function submitData(ev) {
+				if (localStorage.getItem('submited')) {
+					pg.dialog({
+						content: '信息已提交！非常感谢！'
+					});
+					return false;
+				}
+				var _self = this;
+				var sBtn = $(ev.currentTarget);
+				var aInput = sBtn.siblings('ul').find('input');
+				var sBool = this.infosIsNotEmpty(aInput);
+				var needRegLen = aInput.filter(function (i, e) {
+					return $(e).attr('data-regtype') !== '';
+				}).length;
+				var isBreak,
+				    lenCount = 0,
+				    regCount = 0;
+				if (sBool) {
+					//验证信息
+					aInput.each($.proxy(function (i, e) {
+						var $e = $(e);
+						var val = $e.val();
+						var len = val.length;
+						var type = $.trim($e.attr('data-regtype'));
+						switch (type) {
+							case 'name':
+								var nameBool = this.nameReg.test(val);
+								if (nameBool) {
+									regCount++;
+								} else {
+									pg.dialog({
+										content: '请输入正确的姓名！'
+									});
+									isBreak = true;
+								}
+								break;
+							case 'tel':
+								var telRegBool = this.telReg.test(val);
+								if (telRegBool) {
+									if (val.charAt(0) === '1') {
+										if (val.length > 11) {
+											pg.dialog({
+												content: '请输入正确的手机号码！'
+											});
+											isBreak = true;
+										} else {
+											regCount++;
+										}
+									} else if (val.charAt(0) === '0') {
+										var subNumArr = [10, 11, 12];
+										$.each(subNumArr, function (i, v) {
+											if (len === v) {
+												lenCount++;
+											}
+										});
+										if (lenCount === 0) {
+											pg.dialog({
+												content: '请输入正确的座机号码！'
+											});
+											isBreak = true;
+										} else {
+											regCount++;
+										}
+									}
+								} else {
+									pg.dialog({
+										content: '请输入正确的电话号码！'
+									});
+									isBreak = true;
+								}
+								break;
+						}
+						if (isBreak) {
+							return false;
+						}
+					}, this));
+				} else {
+					aInput.each(function (i, e) {
+						var $e = $(e);
+						var title = $e.parent().siblings('p').children('span:eq(0)').text();
+						if ($e.val() === '') {
+							pg.dialog({
+								content: '请输入"' + title + '"信息'
+							});
+							return false;
+						}
+					});
+				}
+				var answerStr = '';
+				$.each(this.selectedData, function (i, item) {
+					$.each(item, function (k, v) {
+						answerStr += i + '-' + k + ':' + v + ',';
+					});
+					answerStr += ';';
+					answerStr = answerStr.replace(/\,\;$/, ';');
+				});
+				var answerBool = this.selectedAnswerIsNotEmpty(); //答案一个都不能为空，必选
+				if (!answerBool) {
+					pg.dialog({
+						content: '每个问卷答案都要选择哦！'
+					});
+					return false;
+				}
+				/*提交*/
+				if (regCount === needRegLen && answerBool) {
+					pg.dsWaitLoading._show('正在提交');
+					var inputInfo = aInput.map(function (i, e) {
+						//表单信息
+						return $(e).val();
+					}).get();
+					$.ajax({
+						url: 'http://172.16.1.39:8080/blog_question/submitQuestionResult',
+						type: 'POST',
+						data: {
+							corp_blog_name: inputInfo[3],
+							corp_name: inputInfo[2],
+							corp_tel: inputInfo[1],
+							username: inputInfo[0],
+							question_ansower: answerStr
+						},
+						dataType: 'json'
+					}).done(function (data) {
+						var sTimer = setTimeout(function () {
+							clearTimeout(sTimer);
+							localStorage.setItem('submited', 'submited');
+							pg.dsWaitLoading._hide();
+							pg.dialog({
+								content: '提交成功！'
+							});
+						}, 3000);
+					}).fail(function (err) {
+						pg.dsWaitLoading._hide();
+						pg.dialog({
+							content: '提交失败！'
+						});
+					});
+				}
+			}
+		}
 	});
 });
 
